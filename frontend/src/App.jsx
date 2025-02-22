351dbb61c67574092f01a1ccd95127fae06a5b7e
taylorvelvet <161587587+taylorvelvet@users.noreply.github.com>
2025-02-22 19:31:14 +0000

    pedals

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,59 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
-  const webSocket = new WebSocket('ws://localhost:3000')
+  const connection = useRef(0)
 
-  const [inputData, setInputData] = useState(null);
+  useEffect(() => {
+    const webSocket = new WebSocket('ws://localhost:3000')
+    connection.current = webSocket;
+  }, [])
 
   const [x, setX] = useState("");
   const [y, setY] = useState("");
   const [z, setZ] = useState("");
   const [acc, setAcc] = useState(false)
+  const [breaking, setBreaking] = useState(false);
 
   function handleMotionEvent(event) {
 
     // console.log("handle motion event", event);
+    
 
     var x = event.accelerationIncludingGravity.x;
     var y = event.accelerationIncludingGravity.y;
     var z = event.accelerationIncludingGravity.z;
 
-
     setX(x);
     setY(y);
     setZ(z);
 
+    const data = {
+      x: x,
+      y: y,
+      z: z,
+      acc: acc,
+      breaking: breaking
+    }
+
+    if (connection.current) {
+      try {
+        connection.current.send(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  function handleActivate() {
+    setAcc(true);
+  }
+
+  function handleDeactivate() {
+    setAcc(false);
   }
 
   useEffect(() => {
@@ -35,14 +62,24 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Device Motion</h1>
-      <div>
+      <div className="pedals">
+        <div className="brake">
+          <img src="src/assets/brakepedal.png" alt=""/>
+        </div>
+        <div className="accelerate" onMouseDown={handleActivate} onMouseLeave={handleDeactivate} onMouseUp={handleDeactivate}>
+          <img src="src/assets/gaspedal.png" alt="" />
+        </div>
+      </div>
+
+      {/* <h1>Device Motion</h1> */}
+      
+      {/* <div>
         X - {x} Y - {y} Z - {z}
       </div>
-      <button>Accelerate</button>
-      <button>Break</button>
-    </div>
-  );
+      <button onMouseDown={handleActivate} onMouseUp={handleDeactivate} onMouseLeave={handleDeactivate}>Accelerate</button>
+      <button>Break</button> */}
+      </div>
+  )
 }
 
 export default App
