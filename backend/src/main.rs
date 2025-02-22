cf03b3e7b05778b79c9f0e84e598ff3368fedb1e
j0 <me@j0.lol>
2025-02-22 14:48:26 +0000

    inital axum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use axum::Router;
+use axum::routing::get;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello HackSussex!"
 }
