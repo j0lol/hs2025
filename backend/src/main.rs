cf6d2c5a7b568ea74a1225adea92191f6d6197f3
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2025-02-22 16:59:32 +0000

    First bevy slop.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,78 +1,26 @@
-use std::error::Error;
-use axum::extract::WebSocketUpgrade;
-use axum::extract::ws::WebSocket;
-use axum::response::{Html, Response};
-use axum::{Extension, Router};
-use axum::routing::{any, get};
-use serde::{Deserialize, Serialize};
-use tokio::net::TcpListener;
+use bevy::prelude::*;
 
-#[derive(Clone, Debug)]
-struct Player {}
-
-#[derive(Clone, Debug)]
-struct State {
-    players: Vec<Player>
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        //.add_systems(Update, ())
+        .run();
 }
 
-impl State {
-    fn new() -> Self {
-        State { players: vec![] }
-    }
-}
+fn setup(mut commands: Commands,
+         mut meshes: ResMut<Assets<Mesh>>,
+         mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(0.25, 0.25, 0.25))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_xyz(0.0, 0.0, 0.0)
+    ));
 
-#[derive(Deserialize, Serialize)]
-#[derive(Debug)]
-enum WsMessage {
-    Accelerometer { content: f64 }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    
-    let state = State::new();
-    
-    let app = Router::new()
-        .route("/ws", any(ws_handler))
-        .route("/testclient", get(test_client))
-        .route("/", get(root))
-        .layer(Extension(state));
-
-    let listener = TcpListener::bind("0.0.0.0:3000").await?;
-    axum::serve(listener, app).await?;
-
-    Ok(())
-}
-
-async fn root(Extension(state): Extension<State>) -> String {
-    format!("{state:?}")
-}
-
-async fn test_client() -> Html<String> {
-    Html(String::from(include_str!("./test.html")))
-}
-
-async fn ws_handler(ws: WebSocketUpgrade) -> Response {
-    ws.on_upgrade(handle_socket)
-}
-
-async fn handle_socket(mut socket: WebSocket) {
-    while let Some(msg) = socket.recv().await {
-        let msg = if let Ok(msg) = msg {
-            msg
-        } else {
-            // client disconnected
-            return;
-        };
-
-        let data = msg.to_text().unwrap();
-        let data: WsMessage = serde_json::from_str(data).unwrap();
-        dbg!(data);
-
-        if socket.send(msg).await.is_err() {
-            // client disconnected
-            return;
-        }
-
-    }
+    // spawn camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 1.0, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ));
 }
