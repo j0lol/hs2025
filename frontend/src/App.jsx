715fa48683d7d80f7151d15227a6bb4b21e4dc94
taylorvelvet <161587587+taylorvelvet@users.noreply.github.com>
2025-02-22 21:31:12 +0000

    steve huckle implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,83 +2,15 @@ import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { isMobile } from 'react-device-detect';
+import ControlsDesktop from './ControlsDesktop';
 
 function App() {
 
-  const connection = useRef(0)
-
-  useEffect(() => {
-    const webSocket = new WebSocket('ws://localhost:3000')
-    connection.current = webSocket;
-  }, [])
-
-  const [x, setX] = useState("");
-  const [y, setY] = useState("");
-  const [z, setZ] = useState("");
-  const [acc, setAcc] = useState(false)
-  const [breaking, setBreaking] = useState(false);
-
-  function handleMotionEvent(event) {
-
-    // console.log("handle motion event", event);
-    
-
-    var x = event.accelerationIncludingGravity.x;
-    var y = event.accelerationIncludingGravity.y;
-    var z = event.accelerationIncludingGravity.z;
-
-    setX(x);
-    setY(y);
-    setZ(z);
-
-    const data = {
-      x: x,
-      y: y,
-      z: z,
-      acc: acc,
-      breaking: breaking
-    }
-
-    if (connection.current) {
-      try {
-        connection.current.send(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }
-
-  function handleActivate() {
-    setAcc(true);
-  }
-
-  function handleDeactivate() {
-    setAcc(false);
-  }
-
-  useEffect(() => {
-    window.addEventListener("devicemotion", handleMotionEvent, true);
-  }, [x, y, z]);
-
   return (
-    <div className="App">
-      <div className="pedals">
-        <div className="brake">
-          <img src="src/assets/brakepedal.png" alt=""/>
-        </div>
-        <div className="accelerate" onMouseDown={handleActivate} onMouseLeave={handleDeactivate} onMouseUp={handleDeactivate}>
-          <img src="src/assets/gaspedal.png" alt="" />
-        </div>
-      </div>
-
-      {/* <h1>Device Motion</h1> */}
-      
-      {/* <div>
-        X - {x} Y - {y} Z - {z}
-      </div>
-      <button onMouseDown={handleActivate} onMouseUp={handleDeactivate} onMouseLeave={handleDeactivate}>Accelerate</button>
-      <button>Break</button> */}
-      </div>
+    <>
+      <ControlsDesktop />
+    </>
   )
 }
 
---
+++ frontend/src/ControlsDesktop.jsx
@@ -0,0 +1,115 @@
+import { useState, useEffect, useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { isMobile } from 'react-device-detect';
+
+export default function ControlsDesktop() {
+
+    const connection = useRef(0)
+    const [device, setDevice] = useState("mouse (around the dot)");
+
+    useEffect(() => {
+        const webSocket = new WebSocket('ws://localhost:3000')
+        connection.current = webSocket;
+        if (isMobile) {
+        setDevice("mobile device")
+        }
+    }, [])
+    
+    const [dir, setDir] = useState(0);
+    const [x, setX] = useState("");
+    const [y, setY] = useState("");
+    const [z, setZ] = useState("");
+    const [acc, setAcc] = useState(false)
+    const [braking, setBraking] = useState(false);
+
+    function calcAngleDegrees(x, y) {
+        return (Math.atan2(y, x) * 180) / Math.PI;
+      }
+
+    function handleMouseMove(event) {
+        let x = event.movementX;
+        let y = event.movementY;
+        let newDir = (Math.atan2(y, x) * 180) / Math.PI;
+
+        setDir(newDir)
+    }
+
+
+
+    function handleMotionEvent(event) {
+        
+        if (isMobile) {
+            
+            var x = event.accelerationIncludingGravity.x;
+            var y = event.accelerationIncludingGravity.y;
+            var z = event.accelerationIncludingGravity.z;
+            
+            setX(x);
+            setY(y);
+            setZ(z);
+            
+            const data = {
+                x: x,
+                y: y,
+                z: z,
+                acc: acc,
+                braking: braking
+            }
+            
+            if (connection.current) {
+                try {
+                    connection.current.send(data);
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
+    }
+        
+    function handlePressBrake() {
+        setBraking(true);
+    }
+
+    function handleReleaseBrake() {
+        setBraking(false);
+    }
+
+    function handleActivate() {
+        setAcc(true);
+    }
+
+    function handleDeactivate() {
+        setAcc(false);
+    }
+
+    useEffect(() => {
+        window.addEventListener("devicemotion", handleMotionEvent, true);
+        window.addEventListener("mousemove", handleMouseMove, true);
+    }, [x, y, z]);
+
+    useEffect(() => {
+        setTimeout(300, () => {
+            console.log('useEffect')
+            if (dir != 0) {
+                setDir(0);
+            }
+        })
+    }, [dir])
+
+    return (
+        <div className="App">
+            {dir}
+            <h1>Use your {device} to steer the car!</h1>
+            <div className="pedals">
+                <div className="brake" onMouseDown={handlePressBrake} onMouseLeave={handleReleaseBrake} onMouseUp={handleReleaseBrake}>
+                <img src="src/assets/brakepedal.png" alt=""/>
+                </div>
+                <div className="accelerate" onMouseDown={handleActivate} onMouseLeave={handleDeactivate} onMouseUp={handleDeactivate}>
+                <img src="src/assets/gaspedal.png" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
