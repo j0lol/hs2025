553f99f96d3e2b6552d905de0a96013f548ce4e3
taylorvelvet <161587587+taylorvelvet@users.noreply.github.com>
2025-02-22 15:13:04 +0000

    Gyro Display

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,32 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useOrientation } from '@uidotdev/usehooks'
 
 function App() {
   const [count, setCount] = useState(0)
+  const orientation = useOrientation();
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Test
-      </p>
+      <h3>
+        Orientation angle:
+        {Object.keys(orientation).map((key) => {
+            return (
+              <tr key={key}>
+                <th>{key}</th>
+                <td>{orientation[key]}</td>
+              </tr>
+            );
+          })}
+      </h3>
     </>
   )
 }
