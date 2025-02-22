dec2e0b89cddfdde2a213dbe61a01439a80dca9d
taylorvelvet <161587587+taylorvelvet@users.noreply.github.com>
2025-02-22 16:53:46 +0000

    https cert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,31 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useOrientation } from '@uidotdev/usehooks'
 
 function App() {
+
+  const webSocket = new WebSocket('ws://localhost:3000')
+
+  const [inputData, setInputData] = useState(null);
+
   const [x, setX] = useState("");
   const [y, setY] = useState("");
   const [z, setZ] = useState("");
+  const [acc, setAcc] = useState(false)
 
   function handleMotionEvent(event) {
-    console.log("handle motion event", event);
+
+    // console.log("handle motion event", event);
 
     var x = event.accelerationIncludingGravity.x;
     var y = event.accelerationIncludingGravity.y;
     var z = event.accelerationIncludingGravity.z;
 
+
     setX(x);
     setY(y);
     setZ(z);
+
   }
 
   useEffect(() => {
@@ -31,6 +39,8 @@ function App() {
       <div>
         X - {x} Y - {y} Z - {z}
       </div>
+      <button>Accelerate</button>
+      <button>Break</button>
     </div>
   );
 }
